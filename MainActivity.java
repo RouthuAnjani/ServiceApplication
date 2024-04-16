903dd9b71e28acf700b3fd13b71429010f99b41b
RouthuAnjani <130381213+RouthuAnjani@users.noreply.github.com>
2024-04-16 16:17:40 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.serviceapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        start=(Button) findViewById(R.id.startButton);
+        stop=(Button) findViewById(R.id.stopButton);
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+    public void onClick(View view) {
+        if(view==start){
+            startService(new Intent(this, MyService.class));
+        } else if (view==stop) {
+            stopService(new Intent(this, MyService.class));
+        }
+    }
+}
